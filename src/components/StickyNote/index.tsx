31b393d2aa4567b6329f9e47b6e32069f0f0638b
Luiz Fernando Santos Silva <luiz.silva@meza.digital>
2021-08-31 00:26:05 -0300

    Adding README.md

--- src/components/StickyNote/index.tsx
+++ src/components/StickyNote/index.tsx
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const StickyNote: React.FC = () => {
-  const [displayNote, setDisplayNote] = useState(false)
+  const [notes, setNotes] = useState(0);
 
-  return !displayNote ? (
-    <button onClick={() => setDisplayNote(true)}>
-      <p className="text-gray-600 dark:text-gray-200 font-roboto py-4 px-4">+ Adicionar nota</p>
-    </button>
-  ) : (
-    <div>
+  const handleNewNote = () => {
+    setNotes(notes + 1);
+  };
+
+  return (
+    <div className="h-full grid grid-cols-4 auto-rows-min gap-1 mt-1 ml-1">
+      {Array.from(Array(notes)).map((_, index) =>
+        <div key={`note-${index}`} className="h-6">
+          <textarea className="outline-none rounded-sm border h-28 dark:bg-gray-400 resize-none" />
+        </div>
+      )}
+      <button className="h-4" onClick={() => handleNewNote()}>
+        <p className="text-gray-600 dark:text-gray-200 font-roboto py-4 px-4">
+          + Adicionar nota
+        </p>
+      </button>
     </div>
   );
-}
+};
 
 export default StickyNote;
